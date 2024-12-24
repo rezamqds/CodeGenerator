35cf29467c7d2d2565ff26224fb1c6a01938de55
Reza Moghaddas <rezamqds@gmail.com>
2024-12-24 21:36:33 +0330

    fix latest changes

--- CodeGen.py
+++ CodeGen.py
@@ -4,20 +4,17 @@ import random
 # f = open(sta , "r")
 # dc_word = f.read()
 
+list = ["apple", "banana", "apricot", "cherry", "date", "fig", "grape", "kiwi", "lemon", "mango", "orange"]
+random.shuffle(list)
 cond = 12
 unique_list = []
 used_f_l = set()
 l_st = []
-
-for word in range (cond):
-    l_st .append(random.Random())
+for i in range(min(cond, len(list))):
+    word = list[i]
     f_l = word[0].lower()
     if f_l not in used_f_l:
         unique_list.append(word)
         used_f_l.add(f_l)
-    print (unique_list)
 
-
-original_list = ["apple", "banana", "apricot", "cherry", "date", "fig", "grape", "kiwi", "lemon", "mango", "orange"]
-result = unique_words(original_list)
-print(result)
+print(unique_list)
