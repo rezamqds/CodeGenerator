29cc9b80e85a8d4697c71bdb99795070931ab13e
Reza Moghaddas <rezamqds@gmail.com>
2024-12-24 21:52:20 +0330

    latest fix

--- CodeGen.py
+++ CodeGen.py
@@ -1,20 +1,19 @@
 import random
 
 # sta = "/storage/emulated/0/python/"+input("file name : ")+".txt"
-# f = open(sta , "r")
-# dc_word = f.read()
-
-list = ["apple", "banana", "apricot", "cherry", "date", "fig", "grape", "kiwi", "lemon", "mango", "orange"]
+with open("C:\\Users\\Admin\\Desktop\\test.txt", "r") as f:
+    dc_word = f.readlines()
+list = [word.strip() for word in dc_word]
 random.shuffle(list)
 cond = 12
-unique_list = []
+unq_l = []
 used_f_l = set()
 l_st = []
 for i in range(min(cond, len(list))):
     word = list[i]
     f_l = word[0].lower()
     if f_l not in used_f_l:
-        unique_list.append(word)
+        unq_l.append(word)
         used_f_l.add(f_l)
 
-print(unique_list)
+print(' '.join(unq_l),end="")
