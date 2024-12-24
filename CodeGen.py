398a80f0583df9cb3e3de315b54bee32120047f5
Reza Moghaddas <rezamqds@gmail.com>
2024-12-24 21:27:34 +0330

    add conditions

--- CodeGen.py
+++ CodeGen.py
@@ -1,17 +1,23 @@
 import random
 
-sta = "/storage/emulated/0/python/"+input("file name : ")+".txt"
+# sta = "/storage/emulated/0/python/"+input("file name : ")+".txt"
 # f = open(sta , "r")
 # dc_word = f.read()
 
-cond = int(input("Len: "))
 cond = 12
-
+unique_list = []
+used_f_l = set()
 l_st = []
-for i in range (cond):
-    l_st .append()
 
-s=(2,5,8)
-s_append = s + (8, 16, 67)
-print(s_append)
-print(s)
+for word in range (cond):
+    l_st .append(random.Random())
+    f_l = word[0].lower()
+    if f_l not in used_f_l:
+        unique_list.append(word)
+        used_f_l.add(f_l)
+    print (unique_list)
+
+
+original_list = ["apple", "banana", "apricot", "cherry", "date", "fig", "grape", "kiwi", "lemon", "mango", "orange"]
+result = unique_words(original_list)
+print(result)
