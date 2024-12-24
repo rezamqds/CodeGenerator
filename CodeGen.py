c9f103225bec988320976fb797a962b372ebbf10
Reza Moghaddas <rezamqds@gmail.com>
2024-12-24 21:12:46 +0330

    add random and list

--- CodeGen.py
+++ CodeGen.py
@@ -1,5 +1,17 @@
+import random
+
 sta = "/storage/emulated/0/python/"+input("file name : ")+".txt"
-print(sta)
-# f = open("r")
-# a = []
-# a.append();
+# f = open(sta , "r")
+# dc_word = f.read()
+
+cond = int(input("Len: "))
+cond = 12
+
+l_st = []
+for i in range (cond):
+    l_st .append()
+
+s=(2,5,8)
+s_append = s + (8, 16, 67)
+print(s_append)
+print(s)
